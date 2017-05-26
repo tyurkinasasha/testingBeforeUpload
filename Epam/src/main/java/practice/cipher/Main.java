246471933b14241a6f0d0b150b28756cd61f3832
tyurkinasasha <tyurkina.sasha@list.ru>
2017-05-26 14:17:56 +0300

    reorg

--- Epam/src/main/java/practice/cipher/Main.java
+++ Epam/src/main/java/practice/cipher/Main.java
@@ -1,11 +1,6 @@
 package practice.cipher;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
