7709ffb3576a121a452d5d4d3421f720d6f3e99b
tyurkinasasha <tyurkina.sasha@list.ru>
2017-05-29 15:51:20 +0300

    unit3

--- Epam/src/main/java/Unit3/Task1/Main.java
+++ Epam/src/main/java/Unit3/Task1/Main.java
@@ -4,4 +4,7 @@ package Unit3.Task1;
  * Created by Tyurkina Alexandra on 5/29/2017.
  */
 public class Main {
+    public static void main(String[] args) {
+
+    }
 }
--- Epam/src/main/java/Unit3/Task2/Main.java
+++ Epam/src/main/java/Unit3/Task2/Main.java
@@ -4,4 +4,7 @@ package Unit3.Task2;
  * Created by Tyurkina Alexandra on 5/29/2017.
  */
 public class Main {
+    public static void main(String[] args) {
+
+    }
 }
